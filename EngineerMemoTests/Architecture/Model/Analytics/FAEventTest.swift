e07b52f6725d8ccc5a52ff22c14520ee82a51a9b
yossibank <yossibank@yahoo.co.jp>
2023-03-27 00:23:06 +0900

    FAテスト追加

--- EngineerMemoTests/Architecture/Model/Analytics/FAEventTest.swift
+++ EngineerMemoTests/Architecture/Model/Analytics/FAEventTest.swift
@@ -7,9 +7,21 @@ final class FAEventTest: XCTestCase {
             FAEvent.screenView.name,
             "画面表示"
         )
+
+        XCTAssertEqual(
+            FAEvent.didTapMemoList(title: "title").name,
+            "メモ一覧タップ"
+        )
     }
 
     func test_FAEvent_parameterの値が返却できてること() {
-        XCTAssertTrue(FAEvent.screenView.parameter.isEmpty)
+        XCTAssertTrue(
+            FAEvent.screenView.parameter.isEmpty
+        )
+
+        XCTAssertEqual(
+            FAEvent.didTapMemoList(title: "title").parameter["メモタイトル"] as! String,
+            "title"
+        )
     }
 }
