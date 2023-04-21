5478b7bd6487e2ae834391e4aefd4d4eebd6195a
yossibank <yossibank@yahoo.co.jp>
2023-04-21 23:47:47 +0900

    開発者用画面APIレスポンス仮表示

--- EngineerMemo/Presentation/Screen/Debug/API/DebugAPIContentView.swift
+++ EngineerMemo/Presentation/Screen/Debug/API/DebugAPIContentView.swift
@@ -7,18 +7,33 @@
     // MARK: - properties & init
 
     final class DebugAPIContentView: UIView {
+        private var cancellables: Set<AnyCancellable> = .init()
+
         private var body: UIView {
             VStackView(alignment: .center) {
-                UILabel().configure {
-                    $0.text = "Hello World!"
+                button.configure {
+                    $0.setTitle("送信", for: .normal)
                 }
+
+                textView
+                    .addConstraint {
+                        $0.width.equalTo(UIScreen.main.bounds.width)
+                        $0.height.equalTo(600)
+                    }
+                    .configure {
+                        $0.font = .boldSystemFont(ofSize: 14)
+                    }
             }
         }
+
+        private let button = UIButton(type: .system)
+        private let textView = UITextView()
 
         override init(frame: CGRect) {
             super.init(frame: frame)
 
             setupView()
+            setupEvent()
         }
 
         @available(*, unavailable)
@@ -27,13 +42,38 @@
         }
     }
 
-    // MARK: - internal methods
-
-    extension DebugAPIContentView {}
-
     // MARK: - private methods
 
-    private extension DebugAPIContentView {}
+    private extension DebugAPIContentView {
+        func setupEvent() {
+            button.publisher(for: .touchUpInside).sink { _ in
+                APIClient().request(
+                    item: DebugGetRequest(
+                        parameters: .init(userId: nil)
+                    )
+                ) { [weak self] result in
+                    switch result {
+                    case let .success(response):
+                        let encoder: JSONEncoder = {
+                            $0.outputFormatting = .prettyPrinted
+                            return $0
+                        }(JSONEncoder())
+
+                        if let data = try? encoder.encode(response),
+                           let json = String(data: data, encoding: .utf8) {
+                            DispatchQueue.main.async {
+                                self?.textView.text = json
+                            }
+                        }
+
+                    case let .failure(error):
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            .store(in: &cancellables)
+        }
+    }
 
     // MARK: - protocol
 
