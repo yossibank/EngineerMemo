91b736c436baf5bc2a1f59c3adfb60cb19a47209
yossibank <yossibank@yahoo.co.jp>
2023-04-29 22:59:14 +0900

    MemoUpdateViewControllerスナップショットテスト作成

---
+++ EngineerMemoTests/TestSnapshot/Snapshot/Memo/MemoUpdateViewControllerSnapshotTest.swift
@@ -0,0 +1,46 @@
+@testable import EngineerMemo
+import iOSSnapshotTestCase
+
+final class MemoUpdateViewControllerSnapshotTest: FBSnapshotTestCase {
+    private var subject: MemoUpdateViewController!
+
+    override func setUp() {
+        super.setUp()
+
+        folderName = "メモ作成・編集画面"
+
+        recordMode = SnapshotTest.recordMode
+    }
+
+    func testMemoUpdateViewController_作成() {
+        snapshot(type: .create)
+    }
+
+    func testMemoUpdateViewController_編集() {
+        snapshot(
+            type: .update(
+                MemoModelObjectBuilder().build()
+            )
+        )
+    }
+
+    func testMemoUpdateViewController_編集_未入力項目あり() {
+        snapshot(
+            type: .update(
+                MemoModelObjectBuilder()
+                    .content(nil)
+                    .build()
+            )
+        )
+    }
+}
+
+private extension MemoUpdateViewControllerSnapshotTest {
+    func snapshot(type: MemoUpdateType) {
+        subject = AppControllers.Memo.Update(type: type)
+
+        snapshotVerifyView(
+            viewMode: .navigation(subject)
+        )
+    }
+}
