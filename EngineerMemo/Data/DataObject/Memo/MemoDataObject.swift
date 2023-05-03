fa5bad2a5dcb0f72f86909081cf87bb12458223d
yossibank <yossibank@yahoo.co.jp>
2023-05-03 21:31:14 +0900

    MemoにCategoryNumberカラム追加

--- EngineerMemo/Data/DataObject/Memo/MemoDataObject.swift
+++ EngineerMemo/Data/DataObject/Memo/MemoDataObject.swift
@@ -2,9 +2,29 @@ import CoreData
 
 @objc(Memo)
 final class Memo: NSManagedObject {
+    @NSManaged var categoryNumber: NSNumber?
     @NSManaged var content: String?
     @NSManaged var identifier: String
     @NSManaged var title: String?
+}
+
+extension Memo {
+    enum Category: Int {
+        case todo
+        case technical
+        case interview
+        case event
+        case other
+    }
+
+    var category: Category? {
+        get {
+            .init(rawValue: categoryNumber?.intValue ?? .invalid)
+        }
+        set {
+            categoryNumber = .init(value: newValue?.rawValue ?? .invalid)
+        }
+    }
 }
 
 extension Memo: Identifiable, IdentifableManagedObject {}
