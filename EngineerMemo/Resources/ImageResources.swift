c7200a069cee16a17b0d6e1aee33fdb5833fc484
yossibank <yossibank@yahoo.co.jp>
2023-05-06 15:38:44 +0900

    プロフィール編集ボタン変更

--- EngineerMemo/Resources/ImageResources.swift
+++ EngineerMemo/Resources/ImageResources.swift
@@ -6,13 +6,4 @@ enum ImageResources {
         static let memo = UIImage(systemName: "list.clipboard.fill")
         static let debug = UIImage(systemName: "gamecontroller")
     }
-
-    enum Profile {
-        static let edit = UIImage(systemName: "person.crop.square")
-    }
-
-    enum Memo {
-        static let add = UIImage(systemName: "plus.app.fill")
-        static let delete = UIImage(systemName: "minus.square.fill")
-    }
 }
