6e046d0ae72c18f1747712ceb700d628bcd8ca2e
yossibank <yossibank@yahoo.co.jp>
2023-07-13 19:30:05 +0900

    create ProjectDetailProcessView

---
+++ EngineerMemo/Presentation/Screen/Profile/Information/Project/Detail/View/ProjectDetailProcessView.swift
@@ -0,0 +1,55 @@
+import UIKit
+import UIKitHelper
+
+// MARK: - properties & init
+
+final class ProjectDetailProcessView: UIView {
+    private var body: UIView {
+        VStackView(alignment: .center) {
+            UILabel().configure {
+                $0.text = "Hello World!"
+            }
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupView()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - internal methods
+
+extension ProjectDetailProcessView {}
+
+// MARK: - private methods
+
+private extension ProjectDetailProcessView {
+    func setupView() {
+        configure {
+            $0.addSubview(body) {
+                $0.edges.equalToSuperview()
+            }
+
+            $0.backgroundColor = .background
+        }
+    }
+}
+
+// MARK: - preview
+
+#if DEBUG
+    import SwiftUI
+
+    struct ProjectDetailProcessViewPreview: PreviewProvider {
+        static var previews: some View {
+            WrapperView(view: ProjectDetailProcessView())
+        }
+    }
+#endif
