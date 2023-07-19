a3e1ef92233e758a33f839564931ffa800823a53
yossibank <yossibank@yahoo.co.jp>
2023-07-20 00:40:16 +0900

    add target widget

---
+++ EngineerMemoWidgets/EngineerMemoWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  EngineerMemoWidgetsBundle.swift
+//  EngineerMemoWidgets
+//
+//  Created by Kamiyama Yoshihito on 2023/07/19.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct EngineerMemoWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        EngineerMemoWidgets()
+    }
+}
