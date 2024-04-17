106f3f5bfa497672ab53b2dc92735da055735dce
yossibank <yossibank@yahoo.co.jp>
2024-04-17 20:13:30 +0900

    update snapshot

--- EngineerMemoSnapshotTests/TestHelper/FBSnapshotTestCase+TestHelper.swift
+++ EngineerMemoSnapshotTests/TestHelper/FBSnapshotTestCase+TestHelper.swift
@@ -84,7 +84,7 @@ private extension FBSnapshotTestCase {
 
         wait(timeout: viewAfter + 3.0) { expectation in
             Task { @MainActor in
-                try await Task.sleep(seconds: 0.5 + viewAfter)
+                try await Task.sleep(seconds: 1.0 + viewAfter)
 
                 FBSnapshotVerifyView(
                     window,
