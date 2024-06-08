a334fca51940636630f5ff34ee2de4d5e64827a9
yossibank <yossibank@yahoo.co.jp>
2024-06-08 10:39:54 +0900

    refactoring data model layer

--- EngineerMemo/Data/DataHolder/DataHolder.swift
+++ EngineerMemo/Data/DataHolder/DataHolder.swift
@@ -1,40 +1,21 @@
-import Foundation
-
 enum DataHolder {
-    @UserDefaultsStorage(
-        .isShowAppReview,
-        defaultValue: false
-    )
+    @UserDefaultsStorage(.isShowAppReview, defaultValue: false)
     static var isShowAppReview: Bool
 
-    @UserDefaultsStorage(
-        .isCoreDataMigrated,
-        defaultValue: false
-    )
+    @UserDefaultsStorage(.isCoreDataMigrated, defaultValue: false)
     static var isCoreDataMigrated: Bool
 
-    @UserDefaultsStorage(
-        .colorTheme,
-        defaultValue: .system
-    )
+    @UserDefaultsStorage(.colorTheme, defaultValue: .system)
     static var colorTheme: DataHolder.ColorTheme
 
-    @UserDefaultsStorage(
-        .profileIcon,
-        defaultValue: .penguin
-    )
+    @UserDefaultsStorage(.profileIcon, defaultValue: .penguin)
     static var profileIcon: DataHolder.ProfileIcon
 
-    @UserDefaultsStorage(
-        .profileProjectSortType,
-        defaultValue: .descending
-    )
+    @UserDefaultsStorage(.profileProjectSortType, defaultValue: .descending)
     static var profileProjectSortType: DataHolder.ProfileProjectSortType
 }
 
 extension DataHolder {
-    @FileStorage(
-        fileName: FileName.someFile.rawValue
-    )
+    @FileStorage(fileName: FileName.someFile.rawValue)
     static var someFile: [String]?
 }
