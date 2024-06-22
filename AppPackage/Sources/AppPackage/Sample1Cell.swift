d9a1d68e363b9f7756f874fc8d338f97ec2d5014
yossibank <yossibank@yahoo.co.jp>
2024-06-22 11:06:54 +0900

    test commit

--- AppPackage/Sources/AppPackage/Sample1Cell.swift
+++ AppPackage/Sources/AppPackage/Sample1Cell.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-class Sample1Cell: UITableViewCell {
+public class Sample1Cell: UITableViewCell {
 
-    override func awakeFromNib() {
+    public override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
+    public override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
